985c0f0a60be35127e9c6d7548aeb92345d64db8
Tomi Rantanen <tomirantanen@users.noreply.github.com>
2019-12-10 18:59:41 +0200

    Heroku setup

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 const app = express();
 
 const bodyParser = require("body-parser");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 morgan.token("body", (request, response) => JSON.stringify(request.body));
 
 app.use(bodyParser.json());
